bbd76ed81a75304edd7b693353cd8c65577e12c2
RajeshSeal <rajesh.seal@gmail.com>
2020-01-22 09:42:49 +1100

    cleanup

--- operationObjects/getRequest.js
+++ operationObjects/getRequest.js
@@ -30,7 +30,7 @@ function getRequest(stnName) {
    
         var vQuery= encodeURI(`TfNSWSF=${m.TfNSWSF}&language=${m.language}&name_sf=${m.name_sf}&outputFormat=${m.outputFormat}&type_sf=${m.type_sf}&version=${m.version}`)
          let url = m.pathURL + vQuery
-        console.log ("the url formed is ", url)       
+        //console.log ("the url formed is ", url)       
         return fetch(url)
             .then((response) => {
                 // console.log(response.json)
